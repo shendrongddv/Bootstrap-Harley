1a30c4ee7b0885090847fa61047fd7f28c406f1c
shendrongddv <shendrongddv@gmail.com>
2022-11-07 23:18:59 +0700

    Initial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 window.onscroll = function () {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    document.getElementById("navbar").classList.add("scrolled", "shadow");
+    document.getElementById("navbar").classList.add("scrolled");
   } else {
-    document.getElementById("navbar").classList.remove("scrolled", "shadow");
+    document.getElementById("navbar").classList.remove("scrolled");
   }
 };
